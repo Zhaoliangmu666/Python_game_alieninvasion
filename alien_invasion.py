13dc529ad34d913071edb27eb50fcf521a7d677b
Zhaoliangmu666 <202413030@hbmzu.edu.cn>
2026-01-02 15:17:36 +0800

    创建一个子弹类，并编组

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from ship import Ship
+from bullet import Bullet
 from settings import Settings
 class AlienInvasion:
     def __init__(self):
@@ -10,8 +11,12 @@ class AlienInvasion:
         # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
+        #设置屏幕尺寸
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        #设置飞船
         self.ship = Ship(self)#调用实例
+        #设置子弹编组
+        self.bullets = pygame.sprite.Group()
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = self.settings.bg_color
 
@@ -19,8 +24,19 @@ class AlienInvasion:
         while True:
             self.check_events()#检查事件，如按键和鼠标事件
             self.update_screen()#更新屏幕
+            self.bullets.update()#更新子弹位置
             self.ship.update()#更新飞船位置
+            self._update_bullets()#删除已消失的子弹
             self.clock.tick(60)
+            
+
+    #更新子弹位置
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        print(len(self.bullets))
 
 
     def check_events(self):
@@ -40,17 +56,27 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+    
+    #开火，创建一颗子弹，并将其加入编组
+    def _fire_bullet(self):
+        if len(self.bullets) < 3:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
            
 
     def update_screen(self):
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         pygame.display.flip()
 
 
