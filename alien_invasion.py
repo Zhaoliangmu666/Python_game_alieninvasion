6da247d4d59999113e3ae3efb04f72c403de5229
Zhaoliangmu666 <202413030@hbmzu.edu.cn>
2026-01-02 13:43:56 +0800

    two

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 class AlienInvasion:
     def __init__(self):
         pygame.init()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230, 230, 230)
@@ -14,6 +15,7 @@ class AlienInvasion:
                     sys.exit()
             self.screen.fill(self.bg_color)
             pygame.display.flip()
+            self.clock.tick(60)
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
